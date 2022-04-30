9191e24008ce1f719c662454243fda407082cee0
nachelysanchez <nashely-5@hotmail.com>
2022-04-30 00:31:27 -0400

    Creando interfaz QuoteApiClient.kt

---
+++ app/src/main/java/com/example/tarea1_ap2/data/network/QuoteApiClient.kt
@@ -0,0 +1,11 @@
+package com.example.tarea1_ap2.data.network
+
+import com.example.tarea1_ap2.data.network.model.QuoteModel
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface QuoteApiClient {
+    @GET("/.json")
+    suspend fun getAllQuotes(): Response<List<QuoteModel>>
+
+}
--- app/src/main/java/com/example/tarea1_ap2/data/network/model/QuoteModel.kt
+++ app/src/main/java/com/example/tarea1_ap2/data/network/model/QuoteModel.kt
@@ -1,4 +1,4 @@
-package com.example.tarea1_ap2.data.model
+package com.example.tarea1_ap2.data.network.model
 
 import com.google.gson.annotations.SerializedName
 
